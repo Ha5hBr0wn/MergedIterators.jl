601ae285c77fee179baaa214cbd383ed81932ee6
Haashim Shah <haashimhshah@gmail.com>
2022-10-28 16:40:34 -0400

    extra benchmark

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,10 @@ const a = SingleIterator{Vector{Int32}, Int32, Int64}(rand(Int32, 10_000_000))
 const b = SingleIterator{Vector{Int8}, Int8, Int64}(rand(Int8, 10_000_000))
 const c = SingleIterator{Vector{Int16}, Int16, Int64}(rand(Int16, 10_000_000))
 
-const mi = MergedIterator(a, b)
+const d = SingleIterator{Vector{Float64}, Float64, Int64}(rand(10_000_000))
+const e = SingleIterator{Vector{Float64}, Float64, Int64}(rand(10_000_000))
+
+const mi = MergedIterator(d, e)
 
 time_merged_iter(m::MergedIterator) = begin
     s = 0.0
@@ -16,4 +19,4 @@ time_merged_iter(m::MergedIterator) = begin
     s
 end
 
-@btime time_merged_iter(mi)
+@btime time_merged_iter($mi)
